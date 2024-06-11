434cfacc96390b224c5145fc065ad2ea7ce38c89
BestBearr <32557819+BestBearr@users.noreply.github.com>
2024-06-11 18:12:03 -0400

    Add: itemize key argument resolver

---
+++ plugin-common/src/main/java/co/crystaldev/itemize/command/ItemKeyArgument.java
@@ -0,0 +1,48 @@
+package co.crystaldev.itemize.command;
+
+import co.crystaldev.alpinecore.framework.command.AlpineArgumentResolver;
+import co.crystaldev.itemize.ItemizePlugin;
+import co.crystaldev.itemize.api.Identifier;
+import dev.rollczi.litecommands.argument.Argument;
+import dev.rollczi.litecommands.argument.parser.ParseResult;
+import dev.rollczi.litecommands.invocation.Invocation;
+import dev.rollczi.litecommands.suggestion.SuggestionContext;
+import dev.rollczi.litecommands.suggestion.SuggestionResult;
+import org.bukkit.command.CommandSender;
+
+/**
+ * @author BestBearr <crumbygames12@gmail.com>
+ * @since 06/11/2024
+ */
+final class ItemKeyArgument extends AlpineArgumentResolver<Identifier> {
+    public ItemKeyArgument() {
+        super(Identifier.class, "itemizeKey");
+    }
+
+    @Override
+    protected ParseResult<Identifier> parse(Invocation<CommandSender> invocation, Argument<Identifier> context, String argument) {
+        int index = argument.indexOf(':');
+        if (index == -1 || index != argument.lastIndexOf(':')) {
+            return ParseResult.failure("<red>Invalid delegate provided");
+        }
+
+        // ensure the key is valid
+        ItemizePlugin itemize = ItemizePlugin.getInstance();
+        Identifier key = Identifier.fromString(argument);
+        if (key == null || !itemize.contains(key)) {
+            return ParseResult.failure("<red>Unregistered or invalid delegate provided");
+        }
+
+        return ParseResult.success(key);
+    }
+
+    @Override
+    public SuggestionResult suggest(Invocation<CommandSender> invocation, Argument<Identifier> argument, SuggestionContext context) {
+        String current = context.getCurrent().lastLevel().toLowerCase();
+        return ItemizePlugin.getInstance().getRegistry().keySet()
+                .stream()
+                .map(Identifier::toString)
+                .filter(v -> v.startsWith(current))
+                .collect(SuggestionResult.collector());
+    }
+}
