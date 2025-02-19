e649dd92630a69461357c021ce38fd7c9d3446ed
BestBearr <32557819+bestbearr@users.noreply.github.com>
2025-02-18 22:28:12 -0500

    fix: literal chance should not be a double

--- plugin-api/src/main/java/co/crystaldev/itemize/api/loot/Chance.java
+++ plugin-api/src/main/java/co/crystaldev/itemize/api/loot/Chance.java
@@ -42,7 +42,7 @@ public final class Chance {
             case CHANCE:
                 return (Math.round((this.value1 * 100.0) * 100.0) / 100.0) + "%";
             default:
-                return this.value1 + "x";
+                return ((int) this.value1) + "x";
         }
     }
 
