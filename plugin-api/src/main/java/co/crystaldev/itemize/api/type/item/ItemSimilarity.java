1976e37ed798f485ec5ec9c6b52156d82362389d
lilyisnotok <92556343+slidesL@users.noreply.github.com>
2025-03-04 17:00:00 -0500

    fix: item durability comparison (desc)
    Eventually should use some other system.

--- plugin-api/src/main/java/co/crystaldev/itemize/api/type/item/ItemSimilarity.java
+++ plugin-api/src/main/java/co/crystaldev/itemize/api/type/item/ItemSimilarity.java
@@ -1,8 +1,8 @@
 package co.crystaldev.itemize.api.type.item;
 
 import co.crystaldev.alpinecore.util.ItemHelper;
-import org.bukkit.entity.Damageable;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Repairable;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -13,17 +13,18 @@ final class ItemSimilarity {
     static boolean isSimilar(@NotNull ItemStack item, @Nullable ItemStack similarItem) {
         // Pretty hacky but we need to ignore durability
 
-        if (similarItem == null) {
+        if (similarItem == null || item.getType() != similarItem.getType()) {
             return false;
         }
 
-        if (item.getType() == similarItem.getType() && (item.getDurability() != similarItem.getDurability() || item.getItemMeta() instanceof Damageable)) {
-            // todo: do we need to compare more than just the name and lore
+        // TODO: eventually use NBT or some other system so we can allow renaming items etc.
+        //  this hack allows enchantments and durability :bruh:
+        if ((item.getItemMeta() instanceof Repairable || item.getDurability() != similarItem.getDurability())) {
             // in what scenario will this not be enough for most custom items?
             // this is also the final iteration of about 2 hours of slamming
             // my head and deffo not the best method. better method is probably
             // just using reflection to access internal comparison tools
-            // or set temporarily durability to the same value for both items
+            // or set temporarily durability to the same value for both -bearr
             return ItemHelper.getDisplayName(item).equals(ItemHelper.getDisplayName(similarItem));
         }
         else {
