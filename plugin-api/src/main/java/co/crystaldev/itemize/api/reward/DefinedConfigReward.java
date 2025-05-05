28c9c27c2447fa53706b7a151d653cf71d145be7
BestBearr <32557819+bestbearr@users.noreply.github.com>
2025-05-05 09:33:15 -0400

    fix: DefinedConfigReward serialization

--- plugin-api/src/main/java/co/crystaldev/itemize/api/reward/DefinedConfigReward.java
+++ plugin-api/src/main/java/co/crystaldev/itemize/api/reward/DefinedConfigReward.java
@@ -8,12 +8,14 @@ import co.crystaldev.itemize.api.loot.Chance;
 import co.crystaldev.itemize.api.reward.type.CommandReward;
 import co.crystaldev.itemize.api.reward.type.ItemizeItemReward;
 import co.crystaldev.itemize.api.reward.type.MultiItemizeItemReward;
-import de.exlll.configlib.Configuration;
+import de.exlll.configlib.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,7 +24,7 @@ import java.util.Map;
  * @since 0.4.0
  */
 @Configuration
-@AllArgsConstructor @NoArgsConstructor
+@NoArgsConstructor
 public final class DefinedConfigReward {
 
     private ConfigMessage displayName;
@@ -31,19 +33,36 @@ public final class DefinedConfigReward {
 
     private RewardType rewardType;
 
-    private Object value;
+    @SerializeWith(serializer = ValueAdapter.class)
+    private ValueHolder value;
+
+    public DefinedConfigReward(@Nullable ConfigMessage displayName, @Nullable ConfigItemizeItem displayItem,
+                                        @NotNull RewardType rewardType, @NotNull Object value) {
+        this.displayName = displayName;
+        this.displayItem = displayItem;
+        this.rewardType = rewardType;
+        this.value = new ValueHolder(value);
+    }
+
+    @PostProcess
+    private void onInit() {
+        if (this.rewardType == null) {
+            this.rewardType = RewardType.ITEM;
+        }
+    }
 
     public @NotNull ItemizeReward build() {
 
         // populate reward values
         LinkedHashMap<String, Chance> values = new LinkedHashMap<>();
-        if (this.value instanceof Map) {
-            ((Map<String, Object>) this.value).forEach((value, amount) -> {
+        Object val = this.value == null ? null : this.value.value;
+        if (val instanceof Map) {
+            ((Map<String, Object>) val).forEach((value, amount) -> {
                 values.put(value, Chance.deserialize(amount));
             });
         }
-        else if (this.value instanceof Iterable) {
-            for (String value : (Iterable<String>) this.value) {
+        else if (val instanceof Iterable) {
+            for (String value : (Iterable<String>) val) {
                 values.put(value, Chance.ONE);
             }
         }
@@ -70,5 +89,45 @@ public final class DefinedConfigReward {
 
     public static @NotNull ItemizeRewardBuilder builder() {
         return new ItemizeRewardBuilder();
+    }
+
+    @Configuration @SerializeWith(serializer = ValueAdapter.class)
+    @AllArgsConstructor
+    static final class ValueHolder {
+        private Object value;
+    }
+
+    static final class ValueAdapter implements Serializer<ValueHolder, Object> {
+        @Override
+        public Object serialize(ValueHolder element) {
+            Object value = element.value;
+
+            if (value instanceof Map) {
+                Map<String, Chance> valueMap = (Map<String, Chance>) value;
+                Map<String, Object> serializedValues = new LinkedHashMap<>();
+                valueMap.forEach((k, v) -> serializedValues.put(k, v.serialize()));
+                return serializedValues;
+            }
+            else if (value instanceof List) {
+                return value;
+            }
+
+            return null;
+        }
+
+        @Override
+        public ValueHolder deserialize(Object element) {
+            if (element instanceof Map) {
+                Map<String, Object> serializedValues = (Map<String, Object>) element;
+                Map<String, Chance> valueMap = new LinkedHashMap<>();
+                serializedValues.forEach((k, v) -> valueMap.put(k, Chance.deserialize(v)));
+                return new ValueHolder(valueMap);
+            }
+            else if (element instanceof List) {
+                return new ValueHolder(element);
+            }
+
+            return null;
+        }
     }
 }
